82b03ab4c87a334383ba496fd09f321ab8a6acda
Rafael Barros de Almeida e Silva <rafa_barros92@hotmail.com>
2022-11-28 22:02:34 -0300

    [feature] Add create account functions + Axios

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -5,8 +5,34 @@ import { ButtonText } from "../../components/ButtonText";
 import Imgbg from "../../assets/img-signIn.png";
 import { FiMail, FiLock, FiUser } from "react-icons/fi"
 import { AiOutlineArrowLeft } from "react-icons/ai"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { api } from "../../services/api";
 
 export function SignUp( ) {
+    const [ email, setEmail ] = useState('');
+    const [ name, setName ] = useState('');
+    const [ password, setPassword ] = useState('');
+    
+    const navigate = useNavigate()
+
+    const handleSingUp = () => {
+        if (!email || !setEmail || !setPassword) {
+            alert("Por favor preencha todos os campos")
+        }
+
+        api.post("/users", {name, email, password}).then(() => {
+            alert("Usuário cadastrado com sucesso")
+            navigate("/")
+        }).catch((error) => {
+            if(error.response) {
+                alert(error.response.data.message);
+            } else {
+                alert("Não foi possível cadastrar!")
+            }
+        })
+    }
+
     return (
         <Container>
             <div className="container-acess">
@@ -18,12 +44,30 @@ export function SignUp( ) {
                         Crie sua conta
                     </p>
 
-                    <Input type="text" placeholder="Nome" icon={FiUser} />
-                    <Input type="email" placeholder="E-mail" icon={FiMail} />
-                    <Input type="password" placeholder="Senha" icon={FiLock} />
+                    <Input 
+                        type="text" 
+                        placeholder="Nome" 
+                        icon={FiUser} 
+                        onChange={e => setName(e.target.value)}
+                    />
+                    <Input 
+                        type="email" 
+                        placeholder="E-mail" 
+                        icon={FiMail} 
+                        onChange={e => setEmail(e.target.value)}    
+                    />
+                    <Input 
+                        type="password" 
+                        placeholder="Senha" 
+                        icon={FiLock} 
+                        onChange={e => setPassword(e.target.value)}
+                    />
 
                     <div className="container-buttons">
-                        <Button title="Entrar" />
+                        <Button 
+                            title="Cadastrar" 
+                            onClick={handleSingUp}
+                        />
                         <ButtonText to="/" title="Voltar para login" icon={AiOutlineArrowLeft} />
                     </div>
                 </div>
